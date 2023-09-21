b12da5fa6701dcac30c6487b85a1021ad3f3cd77
Lu Zhu <zhu.lu@hotmail.com>
2023-09-21 18:51:03 -0400

    wip

--- docs/tutorials/utils.py
+++ docs/tutorials/utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 import umap
 import seaborn as sns
 import datamol as dm
-from sklearn.preprocessing import MinMaxScaler
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -16,12 +15,6 @@ def visualize_chemspace(data: pd.DataFrame, split_names: str, mol_col: str = "sm
     data["UMAP_0"], data["UMAP_1"] = embedding[:, 0], embedding[:, 1]
     for split_name in split_names:
         plt.figure()
-        # if size_col is None:
-        #     size = 10
-        # else:
-        #     scaler = MinMaxScaler((10 , 50))
-        #     size = scaler.fit_transform(data[[size_col]].values).flatten()
-
         fig = sns.scatterplot(data=data, x="UMAP_0", y="UMAP_1", style=size_col, hue=split_name, alpha=0.7)
         fig.set_title(f"UMAP Embedding of compounds for {split_name}")
     return figs
