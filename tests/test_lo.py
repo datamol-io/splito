26140306e445426d2987c7ee971b455e81c3fe48
Simon Steshin <simon.steshin@gmail.com>
2024-04-13 11:10:33 +0400

    Simplify LoSplitter tests

--- tests/test_lo.py
+++ tests/test_lo.py
@@ -6,34 +6,20 @@ from rdkit import Chem, DataStructs
 from splito.lohi import LoSplitter
 
 
-def test_lo():
-    data = dm.solubility()
-    data = data.drop_duplicates(["smiles"])
-
-    smiles = []
-    val = []
-
-    for idx in range(len(data)):
-        if AllChem.MolFromSmiles(data.iloc[idx]["smiles"]):
-            smiles.append(data.iloc[idx]["smiles"])
-            val.append(data.iloc[idx]["SOL"])
-
-    smiles = np.array(smiles)
-    val = np.array(val)
-
+def test_lo(test_dataset_smiles, test_dataset_targets):
     # deafult parameters
     splitter = LoSplitter()
-    train_idx, clusters_idx = splitter.split(smiles, val)
+    train_idx, clusters_idx = splitter.split(test_dataset_smiles, test_dataset_targets)
 
     for cluster in clusters_idx:
-        one_cluster_check(train_idx, cluster, smiles, 0.4, 5, val, 0.60)
+        one_cluster_check(train_idx, cluster, test_dataset_smiles, 0.4, 5, test_dataset_targets, 0.60)
 
     # different parameters
     splitter = LoSplitter(threshold=0.6, min_cluster_size=7, std_threshold=0.8)
-    train_idx, clusters_idx = splitter.split(smiles, val)
+    train_idx, clusters_idx = splitter.split(test_dataset_smiles, test_dataset_targets)
 
     for cluster in clusters_idx:
-        one_cluster_check(train_idx, cluster, smiles, 0.6, 7, val, 0.80)
+        one_cluster_check(train_idx, cluster, test_dataset_smiles, 0.6, 7, test_dataset_targets, 0.80)
 
 
 def one_cluster_check(train_idx, cluster_idx, smiles, threshold, min_cluster_size, values, std_threshold):
@@ -41,29 +27,19 @@ def one_cluster_check(train_idx, cluster_idx, smiles, threshold, min_cluster_siz
 
     # Ensure there is only one similar molecule in the train
     train_smiles = smiles[train_idx]
-    train_mol = [Chem.MolFromSmiles(s) for s in train_smiles]
-    train_fps = [AllChem.GetMorganFingerprintAsBitVect(x, 2, 1024) for x in train_mol]
-
     cluster_smiles = smiles[cluster_idx]
-    cluster_mol = [Chem.MolFromSmiles(s) for s in cluster_smiles]
-    cluster_fps = [AllChem.GetMorganFingerprintAsBitVect(x, 2, 1024) for x in cluster_mol]
+    distance_matrix = dm.similarity.cdist(cluster_smiles, train_smiles, radius=2, nBits=1024)
+    similarity_matrix = 1.0 - distance_matrix
+    is_too_similar = similarity_matrix > threshold
+    no_hits_per_mol = np.sum(is_too_similar, axis=1)
+    assert np.array_equal(no_hits_per_mol, np.ones(len(cluster_smiles), dtype=int))
 
-    hit_indices = set()
-    for cluster_f in cluster_fps:
-        sims = DataStructs.BulkTanimotoSimilarity(cluster_f, train_fps)
-        sims = np.array(sims)
-        assert sum(sims > threshold) == 1
-        hit_indices.add(np.argmax(sims))
+    # Assert the hit is the same for all cluster molecules.
+    hit_indices = np.argmax(is_too_similar, axis=1)
+    assert (hit_indices == hit_indices[0]).all()
 
-    assert len(hit_indices) == 1  # the hit is the same for all cluster
-
-    hit_idx = None
-    for idx in hit_indices:
-        hit_idx = idx
-    hit_smiles = train_smiles[hit_idx]
+    # Verify the variation within the cluster exceeds the threshold
+    hit_smiles = train_smiles[hit_indices[0]]
     hit_idx = list(smiles).index(hit_smiles)
-
-    cluster_values = list(values[cluster_idx]) + [values[hit_idx]]
-    cluster_values = np.array(cluster_values)
-
+    cluster_values = np.append(values[cluster_idx], values[hit_idx])
     assert cluster_values.std() >= std_threshold
